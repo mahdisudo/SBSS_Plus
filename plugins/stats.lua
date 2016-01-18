21c259deeb634abc82aaef78a61d8b362d06db87
Mr Ah S <mrahs@emeil.ir>
2016-01-18 22:12:44 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'Infernal' then -- Put everything you like :)
+  if matches[1]:lower() == 'SBSS' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /Infernal ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /SBSS ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (InfernalTG)",-- Put everything you like :)
-		"^[!/](Infernal)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (SBSS)",-- Put everything you like :)
+		"^[!/](SBSS)"-- Put everything you like :)
     }, 
   run = run
 }
