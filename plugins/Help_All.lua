95a1fbbf09d8c09db01aa651798457badd259a4f
Mr Ah S <mrahs@emeil.ir>
2016-01-18 22:09:41 +0330

    Update Help_All.lua

--- plugins/Help_All.lua
+++ plugins/Help_All.lua
@@ -2,126 +2,125 @@ do
 
 function run(msg, matches)
 return [[ 
-help For all Plugins of INfernalTG :
-1./AddBot [Invitelink] : and you can do it with replay
+دستورات ربات :
+1./AddBot [Invitelink] : اد کردن ربات در گرو بوسیله لینک حتی با ریپلای!
 ======================================================
-2./bot + [botuserID] : allow a bot to join
-3./bot - [botID] : dissallow bot for joining
-4./bot + : enable anti bot in group
-5./bot - : disable in group
+2./bot + [botuserID] : مجاز کزدن یک ربات برای ماندن در گروه
+3./bot - [botID] : حذف دسترسی بات برای ماندن در گروه
+4./bot + : فعال سازی ضد ربات گروه
+5./bot - : غیر فعال کردن ضد بات در گروه
 =====================================================
-6.Auto_Leave : this is not one command , the bot will leave if they add him
+6.Auto_Leave : این یک دستور نیست! ربات بعد از اد شدن در گروه نا مربوط خور بخ خود خارج میشود
 =====================================================
-7./block user [USERID] : block a user !
+7./block user [USERID] : بلاک کردن یک نفر !
 =============================================
-8.whitelist + : enable white list in group
-9.whitelist - : disable whitelist in group
-10.whitelist + [ID] : whitelist user from bot
-11.whitelist - [ID] : unwhitelist user from bot
-12.whitelist group : white list all members of group
-13.whitelist - member {ID} : unwhitelist user when whitelist is enabled
-14.whitelist - group : unwhitelist a group
-15.ban + [@USERNAME] : ban a user when he is in group
-16.ban + [ID] : ban user when he is not in group
-17. ban - [ID] : unban user 
-18.globalban + {ID} :globall ban user from bot [adminonly]
-18.globalban - [ID] :unglobalban user from bot[admin only]
-19.kick [@USERNAME] : kick a user from group
-19.kick [ID] : kick user from group
-dont user "!" and "/"for this plug
+8.whitelist + : فعال سازی لیست سفید در گروه
+9.whitelist - : غیر فعال کردن لیست سفید در گروه
+10.whitelist + [ID] : اضافه کردن یک فرد به لیست سفید
+11.whitelist - [ID] : حذف فرد از لیست سفید
+12.whitelist group : اضافه کردن افراد یک گروه به لیست سفید
+13.whitelist - member {ID} :حذف کردن فرداز لیست سفید گروه 
+14.whitelist - group : حذف افراد یک گروه از لیست سفید
+15.ban + [@USERNAME] :بن کردن فرد وقتی در گروه است
+16.ban + [ID] : بن کردن یک فرد خارج از گزوه
+17. ban - [ID] : آن بن کردن فرد 
+18.globalban + {ID} :سوپر بن کردن فرد
+18.globalban - [ID] :حذف سوپر بن فرد
+19.kick [@USERNAME] : حذف فرد از گروه
+19.kick [ID] : حذف فرد از گروه
+برای اجرای دستورات این پلاگین از / و ! استفاده نکنید
 ======================================================
-20./calc [numb] +or*or/[numb] :Calculator plugin
+20./calc [numb] +or*or/[numb] :ماشین حساب
 ==============================================
-21./Echo [whatever] : return a text
+21./Echo [whatever] : نمایش یک پیام
 ========================================
-22./Feedback [TEXT] : send maseage to @ARASHINFERNAL
+22./Feedback [TEXT] : ارسال نظر به سودو
 =================================================
-23./gps (city)\(ZONE) : find a zone in map
+23./gps (city)\(ZONE) : یافتن یک منطقه در نقشه
 ==============================================
-24./addgroup : add gp to moderation.jsson 
-25./add realm : add group to realm
-26./remgroup : rem gp from moderation.jsson
-27./rem realm : remove gp from realm
-28./rules : shows group rules 
-29./about : shows group description
-30./setname [WHATEVER] : set groups name
-40./setphoto : set group photo
-41./promote [@USERNAME] : Promote a user
-42./promote : promote with reply
-43./clean [modlist][about][member][rules]: clean
-44./kill [chat][realm] remove group and kickall
-45./demote : demote with reply
-46./demote [@USERNAME] : demote user 
-47./set [NAME] [TEXT] : set somthing and get it
+24./addgroup : اضافه کردن مدیریت گروه
+25./add realm : اضافه کردن ریلیم
+26./remgroup : حذف مدیریت گروه
+27./rem realm : حذف ریلیم
+28./rules : نمایش قوانین گروه
+29./about : نمایش توضیحات گروه
+30./setname [WHATEVER] : تنظیم نام گروه
+40./setphoto : تنظیم عکس گروه
+41./promote [@USERNAME] : ارتقا مقام فرد
+42./promote : ارتقا مقام بوسیله ریپلای
+43./clean [modlist][about][member][rules]: پاک کردن  مدیران ، توضیحات ، افراد و یا قوانین گروه
+44./kill [chat][realm] : پاک کردن گروه و حذف همه
+45./demote : از مام فرد با ریپلای
+46./demote [@USERNAME] : ازل مقام فرد
+47./set [NAME] [TEXT] : سیو کردن یک متن و نشان دادن آن
 48./lock [member][name][photo][arabic][leave][bots][flood]
 49./unlock [memb][name][photo][arabic][leave][bots][flood]
-50./setowner {ID} : setowner a user
-51./setowner : setowner with replay
-52./res [@USERNAME] ; get user id
-52./owner : shows group owner
-53./setgpowner [gpID] [ID] : for admins only
-54./setflood [value] : setgroup flood 
-55./settings : shows group setting
-56./modlist :shows group moderators
-57./clink : create a link 
-58./link : get group link
-59./kickinactive :kick group inactivs
-60./kickinactive [ID] ;kick inactive
+50./setowner {ID} : تنظیم فرد به عنوان صاحب گروه
+51./setowner : تنظیم فرد به عنوان صاحب گروه با ریپلای
+52./res [@USERNAME] ; دریافت آیدی کد فرد
+52./owner : نمایش صاحب گروه
+53./setgpowner [gpID] [ID] : فقط برای ادمین ها !
+54./setflood [value] : تنظیم حساسیت به اسپم 
+55./settings : نمایش تنظیمات گروه
+56./modlist :نمایش مدیر های گروه
+57./clink : ساخت/تازه سازی لینک
+58./link : دریافت لینک گروه
+59./kickinactive :کیک کردن افراد غیر فعال
+60./kickinactive [ID] ;کیک کردن افراد غیر فعال
 ===========================================
-61./map ... : get map and location
+61./map ... : دریافت نقشه منطقه
 ==========================================
-62./maseage [ID] [TEXT] : send maseage to user[adminonly]
+62./maseage [ID] [TEXT] : فرستادن پیام برای یک نفر[adminonly]
 ====================================================
-63./globalban @USERNAME : global ban user
-64./globalban : globalban with reply
-65./banlist : get group banslist
-66./ban @USERNAME : ban user 
-67./kick : kick with reply 
-68./ban :BAN with reply
-69./globalunban [ID] : globalunban user
-70./id : get group id , you can do it with replay to
-71./kickme : kick ourself from group
-72./glbanlist : get glbans list
+63./globalban @USERNAME : سوپر بن کردن فرد
+64./globalban : سوپر بن کردن فرد با ریپلای
+65./banlist : لیست افراد بن شده از گروه
+66./ban @USERNAME : بن کردن فرد
+67./kick : کیک کردن با ریپلای 
+68./ban :بن کردن فرد با ریپلای
+69./globalunban [ID] :سوپر بن کردن فرد
+70./id :دریافت آیدی گروه
+71./kickme : خروج از گروه
+72./glbanlist : دریافت لیست افراد سوپر بن شده
 ============================================
-73./S2A [TXT] : send maseage to all
-74./bcgp [ID] [TEXT] : send maseage to gp
+73./S2A [TXT] : ارسال پیام به پی وی همه افراد گروه
+74./bcgp [ID] [TEXT] : فرستادن پیام به گروه
 ====================================
-75./sudoers : return bot sudos
+75./sudo : سودو های ربات
 =====================================
-76./time [city] : get city time
+76./time [city] : دریافت زمان شهر
 ====================================
-77./version : return bot version
+77./version : نمایش ورژن ربات
 ===========================================
-78./all : return all thins about gp
+78./all : همه چیز درباره گروه
 =========================================
-79./cgroup [id] : close group
+79./cgroup [id] : بستن گروه
 =======================================
-80./dic [word] : return word in persian
+80./dic [word] : نمایش معنی کلمه به فارسی
 ===========================================
-81./plug : return all plugins list
-82./plug + [NAME] : enable a plugin
-83./plug - [NAME] : disable a plugin
-84./plug - [NAME] gp : disable a plugin in gp
-85./plug + [NAME] gp :enable a plugin in group
+81./plug : نمایش لیست پلاگین ها
+82./plug + [NAME] : فعال کردن پلاگین
+83./plug - [NAME] : غیر فعال کردن پلاگین
+84./plug - [NAME] gp : غیر فعال کردن پلاگین در گروه 
+85./plug + [NAME] gp :فعال کردن پلاگین در گروه
 ==========================================
-86./stats : send group stats in file.txt
-87./stats InfernalTG : stats bot
-89./infernal : bot about
-90./stats group [ID] : get gp stats
+86./stats : آمار گروه در قالب یک فایل 
+87./stats SBSS : آمار ربات
+89./SBSS : درباره ربات
+90./stats group [ID] : دریافت آمار گروه
 =================================
-91./tagall [txt] : will tagall users
+91./tagall [txt] : تگ کردن افراد گروه و فرستادن پیام برای آنها
 ===============================
-92./kickall [ID] : kickall members from gp
+92./kickall [ID] : حذف همه افراد گروه
 =========================================
-93./toengsupport : return support link
-94./topersupport : return persian support link
+93./toengsupport : لینک ساپورت
 ================================================== 
-95./txt2img [TXT] : text to image 
+94./txt2img [TXT] : تبدیل متن به عکس
 ===========================================
 more helps soon
 you can use both "!" and "/"
-our channell : @INFERNALTEAMCH
-Master admin : @ArashInfernal
+our channell : @SBSS_Team
+Master admin : @Mr_Ah_S
 ]]
 end
 
